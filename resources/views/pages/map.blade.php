78ce0680a8e12a331bdc2b6939e17f20a59e019d
defloyder <99664746+defloyder@users.noreply.github.com>
2026-05-26 16:16:04 +0300

    Landing - Fix Styles

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -24,9 +24,8 @@
             <div class="top-panel__actions">
                 <button class="icon-button" type="button" data-action="locate-me" aria-label="Определить мое местоположение">
                     <svg viewBox="0 0 24 24" aria-hidden="true">
-                        <circle cx="12" cy="12" r="3"></circle>
-                        <path d="M12 2v3M12 19v3M2 12h3M19 12h3"></path>
-                        <path d="M5 5l2.2 2.2M16.8 16.8 19 19M19 5l-2.2 2.2M7.2 16.8 5 19"></path>
+                        <path d="M12 3 20 21l-8-4-8 4 8-18Z"></path>
+                        <path d="M12 3v14"></path>
                     </svg>
                 </button>
                 <button class="icon-button" type="button" data-action="open-search" aria-label="Открыть поиск">
